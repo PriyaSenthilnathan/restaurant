7e5279fc5361711bab43e44eb139a9e51a1bac45
priya6804 <spriyadarshini.22cse@kongu.edu>
2025-05-10 22:30:31 +0530

    frontend completed

---
+++ FRONTEND/components/Footer.jsx
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <div className="container">
+            <div className="banner">
+                <div className="left">Taj Flame</div>
+                <div className="right">
+                    <p>Z6 Gulshn-e-Maymar, Karachi</p>
+                    <p>Open: 10:00 AM - 8:00 PM</p>
+                </div>
+            </div>
+            <div className="banner">
+                <div className="left">
+                    <p>Developed By Taj Flame</p>
+                </div>
+                <div className="right">
+                    <p>All Rights Reserved BY Taj Flame.</p>
+                </div>
+            </div>
+        </div>
+      
+    </footer>
+  )
+}
+
+export default Footer
